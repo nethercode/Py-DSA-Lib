fbcf8dca521efb6b51329ecad2e022729a9e2139
Nethercode <github-user@hotmail.com>
2023-09-28 02:01:09 -0500

    Convert dataset from csv to SQLite db and create utils

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ def exampleUsage_01():
         [90, 32, 68],
     ]
     list_file = mergeSortedLists(sorted_lists)
+    """
     print("Parallel Merge List:", list_file)
     file_path = "./dataset/example_list.txt"
     with open(file_path, "w") as file:
         for item in list_file:
             file.write(str(item) + "\n")
+    """
 
 def exampleUsage_02():
-    target_element = 83
+    target_element = 290000
     result = linearSearch(list_file, target_element)
     if result != -1:
         print(f"Linear Search Result: Element {target_element} found at index {result}.")
@@ -22,7 +24,7 @@ def exampleUsage_02():
         print(f"Linear Search Result: Element {target_element} not found in the list.")
 
 def exampleUsage_03():
-    target_element = 54
+    target_element = 112000
     result = binarySearch(list_file, target_element)
     if result != -1:
         print(f"Binary Search Result: Element {target_element} found at index {result}.")
@@ -31,45 +33,45 @@ def exampleUsage_03():
 
 def exampleUsage_04():
     bubble_sorted_list = bubbleSort(list_file)
-    print("Bubble Sort List:", bubble_sorted_list)
+    # print("Bubble Sort List:", bubble_sorted_list)
 
 def exampleUsage_05():
     insertion_sorted_list = insertionSort(list_file)
-    print("Insertion Sort List:", insertion_sorted_list)
+    # print("Insertion Sort List:", insertion_sorted_list)
 
 def exampleUsage_06():
     with open(list_file, 'r') as file:
         numbers = [int(line.strip()) for line in file]
     quickSort(numbers)
-    print("Quick Sort List:", numbers)
+    # print("Quick Sort List:", numbers)
 
 def exampleUsage_07():
     with open(list_file, 'r') as file:
         numbers = [int(line.strip()) for line in file]
     merge_sorted_list = mergeSort(numbers)
-    print("Merge Sort List:", merge_sorted_list)
+    # print("Merge Sort List:", merge_sorted_list)
 
 def exampleUsage_08():
     with open(list_file, 'r') as file:
         numbers = [int(line.strip()) for line in file]
     selectionSort(numbers)
-    print("Selection Sort List:", numbers)
+    # print("Selection Sort List:", numbers)
 
 def exampleUsage_09():
     with open(list_file, 'r') as file:
         numbers = [int(line.strip()) for line in file]
     heapSort(numbers)
-    print("Heap Sort List:", numbers)
+    # print("Heap Sort List:", numbers)
 
 def exampleUsage_10():
     list1 = [13, 71, 39]
     list2 = [42, 54, 85]
     merged_list = iterativeMerge(list1, list2)
-    print("Iterative Merge List:", merged_list)
+    # print("Iterative Merge List:", merged_list)
 
 if __name__ == "__main__":
     logMessage("START")
-    list_file = './dataset/example_list.txt'
+    list_file = './dataset/output.txt'
     exampleUsage_01()
     exampleUsage_02()
     exampleUsage_03()
